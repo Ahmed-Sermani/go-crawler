8feca3b54da4a7f5b1600306486bd56d09b3af3e
Ahmed Sermani <33938701+Ahmed-Sermani@users.noreply.github.com>
2022-07-12 15:00:21 +0300

    BSP kickoff & message queue interface

---
+++ bsp/bsp.go
@@ -0,0 +1,5 @@
+/*
+   implemeted the BSP https://en.wikipedia.org/wiki/Bulk_synchronous_parallel computing model to aid in
+   processing graph data
+*/
+package bsp
---
+++ bsp/message/message.go
@@ -0,0 +1,23 @@
+/*
+	Message queue
+*/
+package message
+
+type Message interface {
+	Type() string
+}
+
+type Queue interface {
+	Close() error
+	Enqueue(msg Message) error
+	PendingMessages() bool  // indicates that the queue has pending messages
+	DiscardMessages() error // Drop all pending messages in the queue
+	Messages() Iterator
+}
+
+type Iterator interface {
+	// advances the Iterator. if no more messages or an error occure it returns false.
+	Next() bool
+	Message() Message
+	Error() error
+}
