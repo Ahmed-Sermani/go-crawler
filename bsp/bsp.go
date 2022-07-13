61168c2ce587a22b18784eee9cb226f7e38d1a33
Ahmed Sermani <33938701+Ahmed-Sermani@users.noreply.github.com>
2022-07-13 16:16:39 +0300

    Aggreator interface Def & connecting it to Graph

--- bsp/bsp.go
+++ bsp/bsp.go
@@ -5,88 +5,29 @@
 package bsp
 
 import (
-	"github.com/Ahmed-Sermani/go-crawler/bsp/message"
 	"golang.org/x/xerrors"
 )
 
 var ErrUnknownEdgeSource = xerrors.New("source vertex is not part of the graph")
 
-type Vertex[VT any, ET any] struct {
-	id     string
-	value  VT
-	active bool
-	// two queues are needed
-	// one queue to hold the messages for the current super-step
-	// and another queue to buffer the messages for the next super-step
-	// The queue at index super-step%2 hold the messages for the current super-step
-	// the queue at (super-step + 1)%2 buffer the messages for the next super-step
-	msgQueue [2]message.Queue
-	edges    []*Edge[ET]
+type Aggregator interface {
+	Type() string
+	Set(val any)
+	Get() any
+	// updates the Aggregator value based on the current value.
+	Aggregate(val any)
+	
+	// Delta returns the change in the aggregator's value since the last
+	// call to Delta. The delta values can be used in distributed
+	// aggregator use-cases to reduce local, partially-aggregated values
+	// into a single value across by feeding them into the Aggregate method
+	// of a top-level aggregator.
+	//
+	// For example, in a distributed counter scenario, each node maintains
+	// its own local counter instance. At the end of each step, the master
+	// node calls delta on each local counter and aggregates the values
+	// to obtain the correct total which is then pushed back to the workers.
+	Delta() any
+
 }
 
-func (v *Vertex[VT, ET]) ID() string { return v.id }
-
-func (v *Vertex[VT, ET]) Edges() []*Edge[ET] { return v.edges }
-
-// Freeze marks the vertex as inactive. Inactive vertices will not be processed
-// in the following supersteps unless they receive a message in which case they
-// will be re-activated.
-func (v *Vertex[VT, ET]) Freeze() { v.active = false }
-
-func (v *Vertex[VT, ET]) Value() VT { return v.value }
-
-func (v *Vertex[VT, ET]) SetValue(val VT) { v.value = val }
-
-type Edge[ET any] struct {
-	value ET
-	dstID string
-}
-
-func (e *Edge[ET]) DstID() string { return e.dstID }
-
-func (e *Edge[ET]) Value() ET { return e.value }
-
-func (e *Edge[ET]) SetValue(val ET) { e.value = val }
-
-// Graph implements a parallel graph processor based on the concepts described
-// in the Pregel paper https://15799.courses.cs.cmu.edu/fall2013/static/papers/p135-malewicz.pdf .
-type Graph[VT, ET any] struct {
-	superstep    int
-	vertices     map[string]*Vertex[VT, ET]
-	queueFactory message.QueueFactory
-}
-
-// AddVertex inserts a new vertex with the specified id and initial value into
-// the graph. If the vertex already exists, AddVertex will just overwrite its
-// value with the provided initValue.
-func (g *Graph[VT, ET]) AddVertex(id string, initValue VT) {
-	v := g.vertices[id]
-	if v == nil {
-		v = &Vertex[VT, ET]{
-			id: id,
-			msgQueue: [2]message.Queue{
-				g.queueFactory(),
-				g.queueFactory(),
-			},
-			active: true,
-		}
-		g.vertices[id] = v
-	}
-	v.SetValue(initValue)
-}
-
-// AddEdge inserts a directed edge from src to destination and annotates it
-// with the specified initValue. By design, edges are owned by the source
-// and therefore srcID must resolve to a local vertex. Otherwise, AddEdge returns an error.
-func (g *Graph[VT, ET]) AddEdge(srcID, dstID string, initValue ET) error {
-	srcVertex := g.vertices[srcID]
-	if srcVertex == nil {
-		return xerrors.Errorf("create edge from %q to %q: %w", srcID, dstID, ErrUnknownEdgeSource)
-	}
-
-	srcVertex.edges = append(srcVertex.edges, &Edge[ET]{
-		dstID: dstID,
-		value: initValue,
-	})
-	return nil
-}
---
+++ bsp/graph.go
@@ -0,0 +1,97 @@
+package bsp
+
+import (
+	"github.com/Ahmed-Sermani/go-crawler/bsp/message"
+	"golang.org/x/xerrors"
+)
+
+type Vertex[VT any, ET any] struct {
+	id     string
+	value  VT
+	active bool
+	// two queues are needed
+	// one queue to hold the messages for the current super-step
+	// and another queue to buffer the messages for the next super-step
+	// The queue at index super-step%2 hold the messages for the current super-step
+	// the queue at (super-step + 1)%2 buffer the messages for the next super-step
+	msgQueue [2]message.Queue
+	edges    []*Edge[ET]
+}
+
+func (v *Vertex[VT, ET]) ID() string { return v.id }
+
+func (v *Vertex[VT, ET]) Edges() []*Edge[ET] { return v.edges }
+
+// Freeze marks the vertex as inactive. Inactive vertices will not be processed
+// in the following supersteps unless they receive a message in which case they
+// will be re-activated.
+func (v *Vertex[VT, ET]) Freeze() { v.active = false }
+
+func (v *Vertex[VT, ET]) Value() VT { return v.value }
+
+func (v *Vertex[VT, ET]) SetValue(val VT) { v.value = val }
+
+type Edge[ET any] struct {
+	value ET
+	dstID string
+}
+
+func (e *Edge[ET]) DstID() string { return e.dstID }
+
+func (e *Edge[ET]) Value() ET { return e.value }
+
+func (e *Edge[ET]) SetValue(val ET) { e.value = val }
+
+// Graph implements a parallel graph processor based on the concepts described
+// in the Pregel paper https://15799.courses.cs.cmu.edu/fall2013/static/papers/p135-malewicz.pdf .
+type Graph[VT, ET any] struct {
+	superstep    int
+	vertices     map[string]*Vertex[VT, ET]
+	queueFactory message.QueueFactory
+	aggregators map[string]Aggregator
+}
+
+// AddVertex inserts a new vertex with the specified id and initial value into
+// the graph. If the vertex already exists, AddVertex will just overwrite its
+// value with the provided initValue.
+func (g *Graph[VT, ET]) AddVertex(id string, initValue VT) {
+	v := g.vertices[id]
+	if v == nil {
+		v = &Vertex[VT, ET]{
+			id: id,
+			msgQueue: [2]message.Queue{
+				g.queueFactory(),
+				g.queueFactory(),
+			},
+			active: true,
+		}
+		g.vertices[id] = v
+	}
+	v.SetValue(initValue)
+}
+
+// AddEdge inserts a directed edge from src to destination and annotates it
+// with the specified initValue. By design, edges are owned by the source
+// and therefore srcID must resolve to a local vertex. Otherwise, AddEdge returns an error.
+func (g *Graph[VT, ET]) AddEdge(srcID, dstID string, initValue ET) error {
+	srcVertex := g.vertices[srcID]
+	if srcVertex == nil {
+		return xerrors.Errorf("create edge from %q to %q: %w", srcID, dstID, ErrUnknownEdgeSource)
+	}
+
+	srcVertex.edges = append(srcVertex.edges, &Edge[ET]{
+		dstID: dstID,
+		value: initValue,
+	})
+	return nil
+}
+
+func (g *Graph[VT, ET]) RegisterAggregator(name string, aggregator Aggregator) {
+	g.aggregators[name] = aggregator
+}
+
+func (g *Graph[VT, ET]) Aggregator(name string) Aggregator {
+	return g.aggregators[name]
+}
+
+func (g *Graph[VT, ET]) Aggregators() map[string]Aggregator { return g.aggregators }
