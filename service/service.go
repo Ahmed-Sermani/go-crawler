ff02eb7fb40c792083ae01d9fe8c5635c6ef43f4
Ahmed Sermani <33938701+Ahmed-Sermani@users.noreply.github.com>
2022-08-06 12:14:03 +0300

    service group

--- service/service.go
+++ service/service.go
@@ -1,6 +1,12 @@
 package service
 
-import "context"
+import (
+	"context"
+	"sync"
+
+	"github.com/hashicorp/go-multierror"
+	"golang.org/x/xerrors"
+)
 
 type Service interface {
 	Name() string
@@ -8,4 +14,41 @@ type Service interface {
 	// Run executes the service and blocks until the context gets cancelled
 	// or an error occurs.
 	Run(context.Context) error
+}
+
+type ServiceGroup []Service
+
+
+// Run executes all Service instances in the group using the provided context.
+// Calls to Run block until all services have completed executing either because
+// the context was cancelled or any of the services reported an error.
+func (g ServiceGroup) Run(ctx context.Context) error {
+	if ctx == nil {
+		ctx = context.Background()
+	}
+	runCtx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	var wg sync.WaitGroup
+	errCh := make(chan error, len(g))
+	wg.Add(len(g))
+	for _, s := range g {
+		go func(s Service) {
+			defer wg.Done()	
+			if err := s.Run(runCtx); err != nil {
+				errCh <- xerrors.Errorf("%s: %w", s.Name(), err)
+				cancel()
+			}
+		}(s)
+	}
+	
+	<-runCtx.Done()
+	wg.Wait()
+	
+	var err error
+	close(errCh)
+	for svcErr := range errCh {
+		err = multierror.Append(err, svcErr)
+	}
+	return err
 }
