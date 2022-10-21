931b4b682c4ed904c31a465af42c7b4c609826c4
Ahmed Sermani <33938701+Ahmed-Sermani@users.noreply.github.com>
2022-10-21 21:50:56 +0300

    fix: migration job

--- hack/cdb-migrations.sh
+++ hack/cdb-migrations.sh
@@ -6,7 +6,7 @@ HOST=$2
 
 echo "CREATE DATABASE IF NOT EXISTS $1; USE $1;" > /tmp/migrations.sql
 cat /migrations/*.up* >> /tmp/migrations.sql
-until cat /tmp/migrations.sql | ./cockroach sql --insecure --echo-sql --host $DB_HOST; do 
+until cat /tmp/migrations.sql | ./cockroach sql --insecure --echo-sql --host $HOST; do 
 	sleep 5; 
 done
 
