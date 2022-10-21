a04afeebb242c5e5b00e2b42d68c2ff3310f15f1
Ahmed Sermani <33938701+Ahmed-Sermani@users.noreply.github.com>
2022-10-21 21:21:32 +0300

    feat: adding ingress controller

--- hack/kind-with-registry.sh
+++ hack/kind-with-registry.sh
@@ -15,6 +15,20 @@ kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 nodes:
 - role: control-plane
+  kubeadmConfigPatches:
+  - |
+    kind: InitConfiguration
+    nodeRegistration:
+      kubeletExtraArgs:
+        node-labels: "ingress-ready=true"
+  extraPortMappings:
+  - containerPort: 80
+    hostPort: 80
+    protocol: TCP
+  - containerPort: 443
+    hostPort: 443
+    protocol: TCP
+
 - role: worker
 - role: worker
 containerdConfigPatches:
@@ -40,4 +54,10 @@ data:
     help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
 EOF
 
+echo "Installing Ingress Controller"
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=90s
